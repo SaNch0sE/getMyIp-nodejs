e09ae41fc006c5f01e28b09632e6b8d313adcdf3
aboyko <arekisanda@protonmail.com>
2021-11-18 20:10:23 +0200

    feat: not undefined anymore!

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const os = require('os');
 
-module.exports = () => os.networkInterfaces().eth0[0].address;
+module.exports = () => {
+    const addr = os.networkInterfaces().eth0[0].address;
+    if (addr) {
+        return addr;
+    }
+    return '0.0.0.0';
+};
